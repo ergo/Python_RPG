2a8e142e577442be7df453708ce3d8e87e48cffd
Wiranek <32655028+Wiranek@users.noreply.github.com>
2018-01-16 21:02:45 +0100

    Postacie
    Heroes ( dictionaries ).

---
+++ Postacie.py
@@ -0,0 +1,37 @@
+#    Some heroes
+#    A way to perform automatic fight sequence until one of heroes is dead
+#    Heroes may want to have some very basic stats (health, agility, strength) and skills (dodge, block)
+#    Heroes may want to be able to use some items that influence stats
+Warrior1 = {
+    "name":"Wiran",
+    "health":100,
+    "strength":10,
+    "agility":10,
+    "block":15,
+    "dodge":15,
+    "backpack":{},
+}
+
+
+Warrior2 = {
+    "name":"Ruby",
+    "health":80,
+    "strength":9,
+    "agility":11,
+    "block":10,
+    "dodge":20,
+    "backpack":{},
+}
+
+
+Warrior3 = {
+    "name":"Wedge",
+    "health":130,
+    "strength":11,
+    "agility":7,
+    "block":20,
+    "dodge":5,
+    "backpack":{},
+}
+
+gladiators = [Warrior1,Warrior2,Warrior3]
