22465f7829205d5a5a20431d75b0b1b1f1068f5f
Marcin Lulek <info@webreactor.eu>
2018-01-24 20:21:38 +0100

    zmiany

--- Walka.py
+++ Walka.py
@@ -8,50 +8,49 @@ from warriors import Warrior
 from random import randint
 
 
-def walka(char1, char2):
-    turn = 1;  # licznik tur
-    while True:
+class Walka(object):
+
+    def walka(self, attacker, defender):
+        turn = 1  # licznik tur
         # walka, wymiana ciosów do smierci
-        print("%s turn." % turn)
-        uderzenie(char1, char2)
-        if char1.hp <= 0 or char2.hp <= 0:
-            break
-        uderzenie(char2, char1)
-        if char1.hp <= 0 or char2.hp <= 0:
-            break
-        turn += 1
 
+        while attacker.is_alive and defender.is_alive:
+            print("%s turn." % turn)
+            attacker, defender = defender, attacker
+            self.uderzenie(attacker, defender)
+            turn += 1
+            if turn > 100:
+                return
 
-def arena(char1, char2):  # funkcja glowna
-    print ("Fight between %s and %s !" % (char1.name, char2.name))
-    walka(char1, char2)
-    print ("Zwyciężca = %s !" % (wygrany(char1, char2)))
-    return True
+    def arena(self, attacker, defender):  # funkcja glowna
+        print ("Fight between %s and %s !" % (attacker.name, defender.name))
+        self.walka(attacker, defender)
+        print ("Zwyciężca = %s !" % (self.wygrany(attacker, defender)))
+        return True
 
+    def uderzenie(self, attacker, defender):  # atak wojownika attacker
+        block = defender.calculate_block()  # szansa na blok
+        dodge = defender.calculate_dodge()  # szansa na unik
+        hit = attacker.calculate_hit(dodge)
+        damage = attacker.calculate_dmg()
 
-def uderzenie(char1, char2):  # atak wojownika char1
-    hit = char1.agility + randint(0, 75)  # szansa na trafienie
-    damage = char1.backpack["weapon"] + char1.strength - char2.backpack["armor"]  # obrazenia
-    block = char2.block + char2.backpack["shield"]  # szansa na blok
-    dodge = char2.dodge + char2.backpack["boots"]  # szansa na unik
+        if hit > dodge:
+            if block > hit / 1.5:
+                defender.recieve_damage(damage / 2)
+                print("%s blocked the hit for half damage! ( %s hp)" % (defender.name, defender.hp))
 
-    if hit > dodge:
-        if block > hit / 1.5:
-            char2.hp -= damage / 2
-            print("%s blocked the hit for half damage! ( %s hp)" % (char2.name, char2.hp))
-
+            else:
+                defender.recieve_damage(damage)
+                print ("%s got hit! ( %s hp)" % (defender.name, defender.hp))
         else:
-            char2.hp -= damage
-            print ("%s got hit! ( %s hp)" % (char2.name, char2.hp))
-    else:
-        print("%s dodged!" % char2.name)
+            print("%s dodged!" % defender.name)
+
+    def wygrany(self, attacker, defender):  # zwraca imie wygranego
+        if attacker.hp > defender.hp:
+            return attacker.name
+        else:
+            return defender.name
 
 
-def wygrany(char1, char2):  # zwraca imie wygranego
-    if char1.hp > char2.hp:
-        return char1.name
-    else:
-        return char2.name
-
-
-arena(Warrior('a'), Warrior('b'))
+fight_inst = Walka()
+fight_inst.arena(Warrior('a'), Warrior('b'))
--- warriors.py
+++ warriors.py
@@ -3,13 +3,14 @@
 #    Heroes may want to have some very basic stats (health, agility, strength) and skills (dodge, block)
 #    Heroes may want to be able to use some items that influence stats
 #    Jakies cos
-
+from random import randint
 import sys
 
 print sys.version_info
 
 
 class Warrior(object):
+
     def __init__(self, name):
         self.name = name
         self.strength = 10
@@ -19,6 +20,28 @@ class Warrior(object):
         self.dodge = 10
         self.backpack = {"weapon": 4, "armor": 2, "boots": 5, "shield": 4}
 
+    def recieve_damage(self, damage):
+        damage = damage - self.backpack["armor"]
+        if damage >= 0:
+            self.hp -= damage
 
-warrior1 = Warrior(name='Wiran')
+    def calculate_dmg(self):
+        return self.strength + self.backpack["weapon"]
 
+    def calculate_hit(self, dodge_value):
+        return self.agility - dodge_value + randint(0, 75)
+
+    def calculate_dodge(self):
+        return self.dodge + self.backpack["boots"]
+
+    def calculate_block(self):
+        return self.block + self.backpack["shield"]
+
+    @property
+    def is_alive(self):
+        return self.hp > 0
+
+    def __repr__(self):
+        return '<Warrior name={}>'.format(self.name)
+
+ziutek = Warrior('wiran')
